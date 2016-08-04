e7ac5721dc6ee3210054bfc8ec90fdf405e85eec
hlenaleal <helenaleal2013@gmail.com>
2016-08-04 10:19:26 -0700

    ground

--- js/canvas.js
+++ js/canvas.js
@@ -1,8 +1,11 @@
 var canvas = document.querySelector('#canvas');
 var context = canvas.getContext("2d");
+var height = 700;
+var width = 800;
+var u = 0;
 
 var xPos = 0;
-var yPos = 400;
+var yPos = height-130;
 
 image = new Image();
 image.src = "js/girl.png";
@@ -10,7 +13,12 @@ image.src = "js/girl.png";
 image.onload = function() {
         setInterval(loop, 1000 / 30);
     }
+grass = new Image();
+grass.src = "grass.jpg";
 
+grass.onload = function() {
+        setInterval(loop, 1000 / 30);
+    }
 
 function move(n){
     if(n.keyCode == 39){
@@ -46,6 +54,14 @@ function gameloop(){
 }
     canvas.width = canvas.width;
     context.drawImage(image, xPos, yPos,100,100);
+    context.drawImage(grass, u, height-40, width*2, 50);
+    context.fillStyle = "#8B4513";
+    context.fillRect(u,height-30,width*2,100);
+    u-=5;
+    if (u >= width){
+        u=-100;
+    }
     context.stroke();
 }
 document.onkeydown = move;
+
