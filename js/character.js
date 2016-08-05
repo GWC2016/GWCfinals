20428a1c29c40628f9b102b96c43a091d17b402e
hlenaleal <helenaleal2013@gmail.com>
2016-08-05 13:07:59 -0700

    boxworking

--- js/character.js
+++ js/character.js
@@ -38,21 +38,33 @@ info.onload = function() {
 var u = 0;
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d"),
-    width = 700,
+    width = 1400,
     height = 450,
     player = {
-      x : width - 700,
-      y : height - 5,
+      x : 0,
+      y : height - 15,
       width : 100,
       height : 100,
       speed: 3,
       velX: 0,
       velY: 0,
-      jumping: false
+      jumping: false,
+      grounded: false
     },
     keys = [],
     friction = 0.8,
     gravity = 0.3;
+var xbox = width/2;
+var ybox = 350;
+var boxes = [];
+
+// dimensions
+boxes.push({
+    x: xbox,
+    y: ybox,
+    width: 60,
+    height: 60
+});
 
 canvas.width = width;
 canvas.height = height;
@@ -63,27 +75,49 @@ function update(){
     if (keys[38] || keys[32]) {
         // up arrow or space
       if(!player.jumping){
-       player.jumping = true;
-       player.velY = -player.speed*2;
-      }
+           player.jumping = true;
+           player.grounded=false;
+           player.velY = -player.speed*2;
+          }
     }
     if (keys[39]) {
-        // right arrow
+            // right arrow
         if (player.velX < player.speed) {
             player.velX++;
-            u-=3
+            }
         }
-    }
     if (keys[37]) {
-        // left arrow
+            // left arrow
         if (player.velX > -player.speed) {
             player.velX--;
+            }
         }
-    }
 
     player.velX *= friction;
-
     player.velY += gravity;
+
+
+    player.grounded = false;
+    for (var i = 0; i < boxes.length; i++) {
+        ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+
+        var dir = colCheck(player, boxes[i]);
+
+        if (dir === "l" || dir === "r") {
+            player.velX = 0;
+            player.jumping = false;
+        } else if (dir === "b") {
+            player.grounded = true;
+            player.jumping = false;
+        } else if (dir === "t") {
+            player.velY *= -1;
+        }
+
+    }
+
+    if(player.grounded){
+         player.velY = 0;
+    }
 
     player.x += player.velX;
     player.y += player.velY;
@@ -99,23 +133,56 @@ function update(){
         player.jumping = false;
     }
 
-  ctx.clearRect(0,0,width,height);
-  ctx.drawImage(japan, player.x+25, player.y-60, 100,100);
-  ctx.drawImage(image, player.x, player.y - 30, player.width, player.height);
-//    ctx.drawImage(grass, u, height-40, width*2, 50);
-    ctx.drawImage(grass, u, canvas.height-40, canvas.width*3, 50);
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+
+    ctx.drawImage(info, xbox, ybox, 60 ,60);
+      ctx.drawImage(japan, player.x+25, player.y-60, 100,100);
+      ctx.drawImage(image, player.x, player.y - 30, player.width, player.height);
+    ctx.drawImage(grass, u, canvas.height-40, canvas.width, 50);
     ctx.fillStyle = "#8B4513";
     ctx.fillRect(u,canvas.height-30,canvas.width*3,100);
-    if (u <=-700){
-        u=0;
-    }
-
-    ctx.drawImage(info, height/4, width/2, 30 ,30);
 
 
+    ctx.fill();
   requestAnimationFrame(update);
 }
 
+function colCheck(shapeA, shapeB) {
+    // get the vectors to check against
+    var vX = (shapeA.x + (shapeA.width / 5)) - (shapeB.x + (shapeB.width / 5)),
+        vY = (shapeA.y + (shapeA.height / 5)) - (shapeB.y + (shapeB.height / 5)),
+        // add the half widths and half heights of the objects
+        hWidths = (shapeA.width / 5) + (shapeB.width / 5),
+        hHeights = (shapeA.height / 2) + (shapeB.height / 100),
+        colDir = null;
+
+    // if the x and y vector are less than the half width or half height, they we must be inside the object, causing a collision
+    if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+        // figures out on which side we are colliding (top, bottom, left, or right)
+        var oX = hWidths - Math.abs(vX),
+            oY = hHeights - Math.abs(vY);
+        if (oX >= oY) {
+            if (vY > 0) {
+                colDir = "t";
+                shapeA.y += oY;
+            } else {
+                colDir = "b";
+                shapeA.y -= oY;
+            }
+        } else {
+            if (vX > 0) {
+                colDir = "l";
+                shapeA.x += oX;
+            } else {
+                colDir = "r";
+                shapeA.x -= oX;
+            }
+        }
+    }
+    return colDir;
+}
 document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
 });
